1cdf910e96a704bf13d2baceb8cb5b072c8bbcf2
Jairo Rojas <jairorojasofficial@gmail.com>
2023-09-14 21:47:09 -0500

    Integración de Tailwind CSS

--- src/pages/carrito.js
+++ src/pages/carrito.js
@@ -1,10 +1,10 @@
 import Layout from '@/components/global/Layout'
 
-export default function carrito() {
+export default function Carrito() {
   return (
     <>
       <Layout>
-        <h1>Carrito...</h1>
+        <h1 className='text-center p-5'>Carrito, próximamente...</h1>
       </Layout>
     </>
   )
